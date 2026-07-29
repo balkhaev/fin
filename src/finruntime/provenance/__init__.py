d08300fc000bff9123eeb1a1ba05005f3f33c3df
Mikhail <m.balkhaev@gmail.com>
2026-07-30 02:47:11 +0300

    Export strategy migration contract

---
+++ src/finruntime/provenance/__init__.py
@@ -0,0 +1,15 @@
+from finruntime.provenance.migration import (
+    MIGRATION_KINDS,
+    MIGRATION_MODES,
+    MIGRATION_STATUSES,
+    StrategyMigrationRecord,
+    parse_strategy_migration,
+)
+
+__all__ = [
+    "MIGRATION_KINDS",
+    "MIGRATION_MODES",
+    "MIGRATION_STATUSES",
+    "StrategyMigrationRecord",
+    "parse_strategy_migration",
+]
